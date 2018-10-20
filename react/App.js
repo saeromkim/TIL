5b59e5c539ae8576d2b3d2810e9e6a0d0deb055d
먼치킨 <ksr24777@gmail.com>
2018-10-20 22:11:56 +0900

    Add files via upload

---
+++ react/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EventPractice from './EventPractice';
+
+class App extends Component {
+  render(){
+    return(
+      <EventPractice></EventPractice>
+      );
+  }
+}
+
+export default App;
