0a09aa9497efbd682e56f5a14dc4b11b70553cc9
먼치킨 <ksr24777@gmail.com>
2019-01-13 18:43:01 +0900

    Create 15990_1, 2, 3더하기5.cc

---
+++ algorithm/bakejoonAlgorithm/15990_1, 2, 3더하기5.cc
@@ -0,0 +1,38 @@
+//d[i][j]=i를 1,2,3의 합으로 나타내는 방법의 수, 마지막에 사용한 수는 j
+#include <stdio.h>
+using namespace std;
+long long d[1000001][4];
+const long long mod = 1000000009LL;
+const int limit = 100000;
+int main(){
+	for(int i=1; i<=limit; i++){
+			if(i-1 >= 0){
+				d[i][1]=d[i-1][2]+d[i-1][3];
+				if(i==1){
+					d[i][1]=1;
+				}
+			}
+			if(i-2>=0){
+				d[i][2]=d[i-2][1]+d[i-2][3];
+				if(i==2){
+					d[i][2]=1;
+				}
+			}		
+			if(i-3>=0){
+				d[i][3]=d[i-3][1]+d[i-3][2];
+				if(i==3){
+					d[i][3]=1;
+				}
+			}
+		d[i][1] %= mod;
+        d[i][2] %= mod;
+        d[i][3] %= mod;
+		}		
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		int  n;
+		scanf("%d",&n);
+		printf("%lld\n",(d[n][1] + d[n][2] + d[n][3])%mod);
+	}
+}
