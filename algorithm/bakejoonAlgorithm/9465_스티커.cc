f8f71f52ca84fe35eb5812bb65f63136ffd8efb1
먼치킨 <ksr24777@gmail.com>
2019-01-15 17:00:31 +0900

    Create 9465_스티커.cc

---
+++ algorithm/bakejoonAlgorithm/9465_스티커.cc
@@ -0,0 +1,37 @@
+//
+#include <iostream>
+#include <stdio.h>
+#define max(a,b) (((a)>(b))?(a):(b))
+using namespace std;
+long long a[100001][2];//점수배열
+long long d[100001][3];//최댓값배열
+int main() {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		int n;
+		scanf("%d",&n);
+		
+		//배열에 점수 넣기
+		for(int i=1; i<=n; i++){
+			scanf("%lld",&a[i][0]);
+		}
+		for(int i=1; i<=n; i++){
+			scanf("%lld",&a[i][1]);
+		}
+		
+		d[0][0]=d[0][1]=d[0][2]=0;
+		for(int i=1; i<=n; i++){
+			d[i][0]=max(d[i-1][0],max(d[i-1][1],d[i-1][2]));
+			d[i][1] = max(d[i-1][0],d[i-1][2])+a[i][0];
+            d[i][2] = max(d[i-1][0],d[i-1][1])+a[i][1];
+		}
+		long long ans=0;
+		for(int i=1; i<=n; i++){
+			ans=max(max(ans,d[i][0]), max(d[i][1],d[i][2]));
+		}
+		printf("%lld\n",ans);
+	}
+	
+	return 0;
+}
