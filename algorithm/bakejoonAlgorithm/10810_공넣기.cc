ddcccc5c3db237607da8ef805cfa0247f2076b23
먼치킨 <ksr24777@gmail.com>
2019-01-19 18:42:29 +0900

    Update 10810_공넣기.cc

--- algorithm/bakejoonAlgorithm/10810_공넣기.cc
+++ algorithm/bakejoonAlgorithm/10810_공넣기.cc
@@ -7,13 +7,13 @@
 using namespace std;
 int main() {
 	int n,m;
-	cin >> n >> m;
+	cin >> n >> m; // 5 4
 
 	vector<int> a(n); //정답 코드에서는 a(n,0) 이렇게 초깃값을 0으로 해줌.
 	int i,j,k;
 	while(m--){
-		cin >> i >> j >> k ;
-		fill(a.begin()+i-1, a.begin()+j, k);
+		cin >> i >> j >> k ; //1 2 3 
+		fill(a.begin()+i-1, a.begin()+j, k); // 0+1-1= 0번째 부터 0+2(인데 2번은 제외) 까지,  3번으로 채우기
 	}
 	for(int x : a){
 		cout << x << ' ';
