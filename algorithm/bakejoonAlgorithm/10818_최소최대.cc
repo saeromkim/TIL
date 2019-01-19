7c1e8b208d3efd71e074cb334de98233d0696026
먼치킨 <ksr24777@gmail.com>
2019-01-19 21:44:33 +0900

    Update 10818_최소최대.cc

--- algorithm/bakejoonAlgorithm/10818_최소최대.cc
+++ algorithm/bakejoonAlgorithm/10818_최소최대.cc
@@ -1,4 +1,29 @@
 /*
+두번째 풀었을때
+min_element/max_element 함수를 이용해서 풀었다.
+이터레이터를 출력할 때 auto형 변수에 넣은 후, *를 이용해서 출력해야함에 유의한다. (바로 출력 불가하다)
+*/
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i=0; i<n; i++){
+		cin >> a[i];
+	}
+	auto min = min_element(a.begin(), a.end());
+	auto max = max_element(a.begin(), a.end());
+	cout << *min << " ";
+	cout << *max << "\n";
+	
+    return 0;
+}
+
+/*
+첫번째 풀었을때
 최소,최대값을 구하는 문젠데 생각보다 조금 헤맸다.
 문제에서 주어진 최소최대값을 min,max에 반대로 초기화하는 것이 첫번째 핵심이고,
 temp로 스와치 하는게 아니라 입력받은 수를 바로바로 min과 max값과 비교하여 넣으면 되는 비교적 간단한 문제다.
