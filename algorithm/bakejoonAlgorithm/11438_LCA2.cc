546990a0aa689e5ed2b5b873c1f6437e2c9faaba
먼치킨 <ksr24777@gmail.com>
2019-02-18 16:02:24 +0900

    LCA2 다이나믹, tin/tout

--- algorithm/bakejoonAlgorithm/11438_LCA2.cc
+++ algorithm/bakejoonAlgorithm/11438_LCA2.cc
@@ -86,3 +86,66 @@ int main(){
 	}
 	return 0;
 }
+/*
+	 tin, tout을 사용해서 풀어본 문제.
+*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+const int MAX = 100111;
+vector<int> a[MAX];
+int p[MAX][18];
+int tin[MAX];
+int tout[MAX];
+int timer;
+int l;
+void dfs(int v, int parent){
+	tin[v] = ++timer;
+	p[v][0] = parent;
+	for(int i=1; i<=l; i++){
+		p[v][i] = p[p[v][i-1]][i-1]; //각 정점의 제곱수만큼의 부모를 구함.(할머니, 할머니의 할머니)
+	}
+	for(int to : a[v]){
+		if(to != parent){
+			dfs(to, v);
+		}
+	}
+	tout[v] = ++timer;
+}
+bool upper(int u, int v){
+	return (tin[u] <= tin[v] && tout[u] >= tout[v]); //
+}
+int lca(int u, int v){
+	if(upper(u,v)) return u;
+	if(upper(v,u)) return v;
+	for(int i=l; i>=0; i--){
+		if(!upper(p[u][i],v)){
+			u = p[u][i];
+		}
+	}
+	return p[u][0];
+}
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	for(int i=0; i<n-1; i++){
+		int u,v;
+		scanf("%d %d",&u,&v);
+		a[u].push_back(v);
+		a[v].push_back(u);
+	}
+	for(l=1; (1<<l)<=n; l++); //n의 log값 구해서 l에 저장
+	l-=1; 
+	dfs(1,1);
+	int m;
+	scanf("%d",&m);
+	while(m--){
+		int u,v;
+		scanf("%d %d",&u,&v);
+		printf("%d\n",lca(u,v));
+	}
+	return 0;
+}
