ee31f25c86bd331fc15ed779fbbedc9b3f1df1f6
먼치킨 <ksr24777@gmail.com>
2019-03-05 23:18:10 +0900

    Update 1929_소수구하기.cc

--- algorithm/bakejoonAlgorithm/1929_소수구하기.cc
+++ algorithm/bakejoonAlgorithm/1929_소수구하기.cc
@@ -1,4 +1,33 @@
 //2부터 n까지의 모든 수의 배수를 지운다
+//2번째 
+#include <iostream>
+#include <cmath>
+#include <bitset>
+using namespace std;
+int go(int n){
+	if(n<2) return false;
+	for(int i=2; i*i<=n; i++){
+		if(n%i==0){
+			return false;
+		}
+	}
+	return true;
+}
+int main() {
+	int m,n;
+	cin>>m>>n;
+	int cnt=0;
+	for(int i=m; i<=n; i++){
+		if(go(i)){
+			cout<<i<<"\n";
+		}
+	}
+	
+	return 0;
+}
+
+
+//처음 풀었을때
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
