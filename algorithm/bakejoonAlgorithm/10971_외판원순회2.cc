bd945a99d3dcd72e268402dca8a78dd3f42df115
김새롬 <ksr24777@gmail.com>
2019-04-09 14:55:31 +0900

    브루트포스 순열 중

--- algorithm/bakejoonAlgorithm/10971_외판원순회2.cc
+++ algorithm/bakejoonAlgorithm/10971_외판원순회2.cc
@@ -1,3 +1,52 @@
+/*
+	이 전과 똑같이 풀었다
+	next_permutation으로 모든 경로에 대해 가장 적은 비용을 구한다.
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int w[11][11]; //주어지는 비용행렬
+
+int main() {
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cin>>w[i][j];
+		}
+	}
+	vector<int> d(n);
+	for(int i=0; i<n; i++){
+		d[i]=i;
+	}
+	int ans =2147483647;
+	
+	//모든 경로에 대해
+	do{
+		bool check = true;
+		int sum = 0;
+		
+		for(int i=0; i<n-1; i++){
+			//거리비용이 있는지 먼저 확인
+			if(w[d[i]][d[i+1]] == 0) {
+				check = false; //0이면 갈 수 없음
+			}
+			else {
+				sum += w[d[i]][d[i+1]]; //비용이 있으면 더함
+			}
+		}
+		//돌아오는 길 구하기
+		if(check == true && w[d[n-1]][d[0]] != 0){ //d[3][0]
+			sum += w[d[n-1]][d[0]];
+			if(ans>sum) ans=sum;
+		}
+	}while(next_permutation(d.begin(), d.end()));
+	
+	cout<<ans;
+	return 0;
+}
+
 //순열 이용 
 #include <iostream>
 #include <algorithm>
