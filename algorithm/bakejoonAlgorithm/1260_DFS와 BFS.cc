3d03d4f84b264d473359a0aacd6307601731dd6d
김새롬 <ksr24777@gmail.com>
2019-04-17 17:09:22 +0900

    dfs, bfs 기본 구현 문제

--- algorithm/bakejoonAlgorithm/1260_DFS와 BFS.cc
+++ algorithm/bakejoonAlgorithm/1260_DFS와 BFS.cc
@@ -1,54 +1,64 @@
-#include <cstdio>
+//인접리스트를 이용한 dfs와 bfs구현하기 문제
+#include <iostream>
 #include <algorithm>
 #include <cstring>
-#include <vector>
 #include <queue>
+#include <vector>
 using namespace std;
+
 vector<int> a[1001];
 bool check[1001];
-void dfs(int node) {
-    check[node] = true;
-    printf("%d ",node);
-    for (int i=0; i<a[node].size(); i++) {
-        int next = a[node][i];
-        if (check[next] == false) {
-            dfs(next);
-        }
-    }
+
+void dfs(int x){
+	check[x] = true;
+	cout<<x<<" ";
+	for(int i=0; i<a[x].size(); i++){
+		int y = a[x][i];
+		if(check[y] == false){
+			dfs(y);
+		}
+	}
 }
+
 void bfs(int start) {
-    queue<int> q;
-    memset(check,false,sizeof(check));
-    check[start] = true;
-    q.push(start);
-    while (!q.empty()) {
-        int node = q.front();
-        q.pop();
-        printf("%d ",node);
-        for (int i=0; i<a[node].size(); i++) {
-            int next = a[node][i];
-            if (check[next] == false) {
-                check[next] = true;
-                q.push(next);
-            }
-        }
-    }
+	//큐 초기화하기
+	queue<int> q;
+	memset(check, false, sizeof(check));
+	check[start] = true;
+	q.push(start);
+	while(!q.empty()){
+		//방문 순서대로 출력하기
+		int x = q.front();
+		q.pop();
+		cout<<x<<" ";
+		//각 정점마다 방문안한 곳 방문하기
+		for(int i=0; i<a[x].size(); i++){
+			int y =  a[x][i];
+			if(check[y] == false){
+				check[y] = true;
+				q.push(y);
+			}
+		}
+	}
 }
+
 int main() {
-    int n, m, start;
-    scanf("%d %d %d",&n,&m,&start);
-    for (int i=0; i<m; i++) {
-        int u,v;
-        scanf("%d %d",&u,&v);
-        a[u].push_back(v);
-        a[v].push_back(u);
-    }
-    for (int i=1; i<=n; i++) {
-        sort(a[i].begin(), a[i].end());
-    }
-    dfs(start);
-    puts("");
-    bfs(start);
-    puts("");
-    return 0;
+	//정점의 개수 n, 간선의 개수 m, 시작 정점 번호 start
+	int n, m, start;
+	cin>>n>>m>>start;
+	for(int i=0; i<m; i++){
+		int u, v;
+		cin>>u>>v;
+		a[u].push_back(v);
+		a[v].push_back(u);
+	}
+	for(int i=1; i<=n; i++){
+		sort(a[i].begin(), a[i].end());
+	}
+	dfs(start);
+	cout<<"\n";
+	bfs(start);
+	cout<<"\n";
+	
+	return 0;
 }
