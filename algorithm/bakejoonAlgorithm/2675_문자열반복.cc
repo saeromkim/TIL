294002add4a0634963b62794fe4fda3e0d4c3575
김새롬 <ksr24777@gmail.com>
2019-04-22 22:22:34 +0900

    문자열 쉬움

---
+++ algorithm/bakejoonAlgorithm/2675_문자열반복.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<n; j++){
+                cout<<s[i];
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
