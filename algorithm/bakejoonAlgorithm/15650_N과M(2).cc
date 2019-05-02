b9865a21e0e43b2fc27025f85cc647834f6035cc
김새롬 <ksr24777@gmail.com>
2019-05-02 19:25:04 +0900

    다시 품 내가

--- algorithm/bakejoonAlgorithm/15650_N과M(2).cc
+++ algorithm/bakejoonAlgorithm/15650_N과M(2).cc
@@ -1,3 +1,33 @@
+//내가 푼거. 다시 품
+#include <iostream>
+using namespace std;
+bool check[10]; //숫자 사용 여부 체크
+int a[10]; //만든 수열을 넣을 배열
+
+void go(int index, int start, int n, int m){ //start라는 인수 추가
+	if(index == m){
+		for(int i=0; i<m; i++){
+			cout<<a[i]<<" ";
+		}
+		cout<<"\n";
+		return;
+	}
+	for(int i=start; i<=n; i++){ //start부터 숫자 넣기
+		if(check[i]) continue;
+		check[i] = true;
+		a[index] = i; 
+		go(index + 1, i+1, n, m); //1을 넣었다면 2를 start로 
+		check[i] = false;
+	}
+}
+
+int main() {
+	int n,m;
+	cin>>n>>m;
+	go(0,1,n,m);
+	return 0;
+}
+
 //n과m(1) 변형
 #include <iostream>
 using namespace std;
